56c0f5f7d2240f4358a7978e46da2082a5e1bcbe
huyhuanhg <huyhuanhg@gmail.com>
2021-09-23 01:17:31 +0700

    update message validate

--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -7,17 +7,17 @@ class UploadImageRequest extends Request
     public function rules()
     {
         return [
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:1024',
         ];
     }
 
     public function messages()
     {
         return [
-            'image.required' => 'k tồn tại',
-            'image.image' => 'k phải ảnh',
-            'image.mimes' => 'k Loại không đúng',
-            'image.max' => 'k quá tải',
+            'image.required' => 'Bạn chưa đăng ảnh nào!',
+            'image.image' => 'Định dạng file phải là ảnh!',
+            'image.mimes' => 'Định dạng ảnh không đúng!',
+            'image.max' => 'Dung lượng ảnh quá lớn!',
         ];
     }
 }
--- app/Http/Requests/UploadImagesRequest.php
+++ app/Http/Requests/UploadImagesRequest.php
@@ -8,17 +8,20 @@ class UploadImagesRequest extends Request
     {
         return [
             'images' => 'required|array|max:8',
-            'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:1024',
         ];
     }
 
     public function messages()
     {
         return [
-            'images.required' => 'k tồn tại',
-            'images.array' => 'k phải ảnh',
-            'images.max' => 'k quá tải',
-            'images.*.image' => 'không phải là ảnh'
+            'images.required' => 'Danh sách ảnh không tồn tại!',
+            'images.array' => 'Dữ liệu tải lên không phải danh sách!',
+            'images.max' => 'Số lượng tải tối đa là 8!',
+            'images.*.required' => 'Định dạng file không phải là ảnh!',
+            'images.*.image' => 'Định dạng file không phải là ảnh!',
+            'images.*.mimes' => 'Định dạng file không đúng!',
+            'images.*.max' => 'Dung lượng ảnh quá lớn!',
         ];
     }
 }
